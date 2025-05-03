5c4c8774537a7db02dcf724a16513266554727e5
Eslam Bashry <eslamhussin600@gmail.com>
2025-05-03 13:36:11 +0300

    get one user and update user

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { forgetPassword, login, logout, resetPassword, signup } from "./auth.controller.js";
+import { forgetPassword, login, logout, resetPassword, signup,getSingleUser, UpdateUser } from "./auth.controller.js";
 
 const userRouter = Router()
 
@@ -8,5 +8,29 @@ userRouter.post('/login', login)
 userRouter.post('/logout',logout)
 userRouter.post('/forgetPassword',forgetPassword)
 userRouter.post('/resetPassword/:token',resetPassword)
+userRouter.post('/getUser/:id',getSingleUser)
+userRouter.put('/update/:id',UpdateUser)
 
 export default userRouter
+
+
+let age = 18;
+
+if (age >= 18 && age < 65) {
+  console.log("Adult");
+} else {
+  console.log("Not an adult");
+}
+
+
+
+
+
+
+
+
+
+
+  
+
+
