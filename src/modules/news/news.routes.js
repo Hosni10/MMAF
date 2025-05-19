a5d04bafc536caa7eeaa1cf0a251a10fe8e93ebe
Eslam Bashry <eslamhussin600@gmail.com>
2025-05-19 23:39:08 +0300

    charts

--- src/modules/news/news.routes.js
+++ src/modules/news/news.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addNews, deleteNews, getNews, getNewsById, getTenNews, updateNews ,deleteNewsImage} from "./news.controller.js";
+import { addNews, deleteNews, getNews, getNewsById, getTenNews, updateNews ,deleteNewsImage, getNewsChartData} from "./news.controller.js";
 import { multerCloudFunction } from "../../../services/multerCloud.js";
 import { allowedExtensions } from "../../utilities/allowedExtensions.js";
 
@@ -15,6 +15,9 @@ newsRouter.get('/getnewsbyid/:id', getNewsById)
 newsRouter.delete('/:id',deleteNews )
 newsRouter.get('/getLastTenNews', getTenNews)
 newsRouter.delete('/deleteNewsImage/:newsId/:imageId',deleteNewsImage)
+newsRouter.get('/getNewsCharts/', getNewsChartData)
+
+
 
 export default newsRouter
 
